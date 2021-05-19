3d63567b3466bf82ec1c9600b9b9a43b39e8fae9
deggaronn <abhishekbondwal@gmail.com>
2021-05-20 02:54:33 +0530

    removed footer

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ session_start();
     </div>
     </div>
 
-    <div class="bottom-container">
+   <!-- <div class="bottom-container">
         <div class="row">
             <div class="footer-col">
                 <a href="#" style="color:white" class="btn">Sign up</a>
@@ -125,7 +125,7 @@ session_start();
                 <a href="#" style="color:white" class="btn">Forgot password?</a>
             </div>
         </div>
-    </div>
+    </div>-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/validation.js"></script>
 </body>
